5e889a69ad86e63f1d2d6fde67a52b5c1787865e
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2022-11-23 23:04:46 +0900

    stylelintの設定

--- digital-agency-design-system-vue/src/App.vue
+++ digital-agency-design-system-vue/src/App.vue
@@ -22,9 +22,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
