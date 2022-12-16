e0079807d7677da1c5a50bb07cf028c277bbed63
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2022-12-16 09:39:44 +0900

    render関数の作成

--- digital-agency-design-system-vue/src/composables/useNameInput.ts
+++ digital-agency-design-system-vue/src/composables/useNameInput.ts
@@ -1,6 +1,22 @@
-import { ref } from "vue";
+import { h, ref } from "vue";
+import TextInput from "../components/TextInput.vue";
 
-
-export const useNameInput = (arg: { label: string; placeholder: string }) => {
-  const name = ref();
+export const useNameInput = (arg: {
+  label: string;
+  placeholder?: string;
+  supportText?: string;
+  errorText?: string;
+  isRequired: boolean;
+}) => {
+  const name = ref<string>();
+  const isChanged = ref<boolean>(false);
+  const render = () =>
+    h(TextInput, {
+      type: "text",
+      modelValue: name,
+      placeholder: arg.placeholder,
+      supportText: arg.supportText,
+      errorText: arg.errorText,
+    });
+  return { name, render };
 };
