5b563d88e1648e70609ecfbc9890fd2e22323f57
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-01-13 21:26:40 +0900

    Update vite.config.ts

--- digital-agency-design-system-vue/vite.config.ts
+++ digital-agency-design-system-vue/vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/digital-agency-design-system/",
   resolve: {
     alias: {
       "@": `${__dirname}/src/`,
