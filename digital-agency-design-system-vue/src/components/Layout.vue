f85242ad474572e863f0fffc1aff20de812be87e
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-03-16 20:46:12 +0900

    レイアウトシステムの基本形作成

---
+++ digital-agency-design-system-vue/src/components/Layout.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="layout">
+    <slot></slot>
+  </div>
+</template>
+<style lang="scss">
+@use "@/assets/style/utils/utils.scss" as *;
+
+.layout {
+  max-width: 1104px;
+  padding: 0 40px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: auto;
+  column-gap: 32px;
+  row-gap: 32px;
+  grid-auto-rows: auto;
+}
+
+@for $i from 1 through 12 {
+  .colspan-#{$i} {
+    grid-column: span $i / span $i;
+  }
+}
+
+@include mediaQueryDown() {
+  .layout {
+    padding: 0 16px;
+  }
+}
+</style>
