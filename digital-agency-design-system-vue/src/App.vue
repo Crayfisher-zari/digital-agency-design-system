b00f3f05471d2eea820b55e8e2e512951ed49c5e
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-06-02 22:58:19 +0900

    レイアウトを変更

--- digital-agency-design-system-vue/src/App.vue
+++ digital-agency-design-system-vue/src/App.vue
@@ -84,219 +84,288 @@ watch(colorScheme, (color) => {
 <template>
   <div class="globalWrapper">
     <Pankuzu :list="pankuzu" />
-
+    <Heading :headingLevel="1" designLevel="XL"> コンポーネント </Heading>
+    <hr />
     <Layout>
       <div class="colSpan-12">
-        <Heading :headingLevel="1" designLevel="XXL">見出しXXL</Heading>
-        <Heading :headingLevel="1" designLevel="XL">見出しXL</Heading>
-        <Heading :headingLevel="1"> 見出しL </Heading>
-        <Heading :headingLevel="2"> 見出しM </Heading>
-        <Heading :headingLevel="3"> 見出しS </Heading>
-        <Heading :headingLevel="4"> 見出しXS </Heading>
-        <Heading :headingLevel="5"> 見出しXXS </Heading>
-      </div>
-      <div class="colSpan-12">
-        <p class="fontTextL">本文L：デフォルトの本文テキスト。</p>
-        <p class="fontTextM">
-          本文M：テキストの多いとき、表示領域が限られているときに使用できます。
-        </p>
-        <p class="fontLabeLL">ラベルL：デフォルトのラベルテキスト。</p>
-        <p class="fontLabelM">
-          ラベルM：テキストの多いとき、表示領域が限られているときに使用できます。
-        </p>
-        <p class="fontSupportL">補足L：デフォルトの補足テキスト。</p>
-        <p class="fontSupportM">
-          補足M：テキストの多いとき、表示領域が限られているときに使用できます。
-        </p>
-      </div>
-      <div class="colSpan-12">
-        <BasicButton label="ボタン" @click="handleClick" />
-        <BasicButton label="ボタン" type="secondary" @click="handleClick" />
-        <BasicButton label="ボタン" type="tertiary" @click="handleClick" />
-      </div>
-    </Layout>
+        <Heading :headingLevel="2"> タイポグラフィ </Heading>
 
-    <div class="colorScheme">
-      <RadioGroup
-        v-model="colorScheme"
-        groupLabel="テーマカラー"
-        :labels="['ライトモード', 'ダークモード']"
-        :values="['light', 'dark']"
-        helpText="サイトのテーマカラーを選択できます"
-        name="color"
-        :isRequired="false"
-      />
-    </div>
-    <div class="inputWrapper">
-      <FamilyNameInput />
-      <UseTelInputComponent />
-      <UseTextAreaInputComponent />
-      <MaxCountTextArea />
-      <div>
-        <RadioGroup
-          v-model="selected"
-          groupLabel="ラベル"
-          :labels="['選択肢1', '選択肢2', '選択肢3']"
-          :values="['1', '2', '3']"
-          helpText="ヘルプテキスト"
-          errorText="エラーテキスト"
-          name="group"
-          :isRequired="true"
-        />
-      </div>
-      <div>
-        <RadioGroup
-          v-model="selected2"
-          radioStyle="tile"
-          groupLabel="ラベル"
-          :labels="['選択肢1', '選択肢2', '選択肢3']"
-          :subTexts="['補足説明1', '補足説明2', '補足説明3']"
-          :values="['1', '2', '3']"
-          helpText="ヘルプテキスト"
-          errorText="エラーテキスト"
-          name="group2"
-          :isRequired="true"
-        />
-      </div>
-      <div>
-        <CheckboxGroup
-          v-model="checked"
-          groupLabel="ラベル"
-          :labels="['選択肢1', '選択肢2', '選択肢3']"
-          :values="['1', '2', '3']"
-          helpText="ヘルプテキスト"
-          errorText="エラーテキスト"
-          name="group3"
-        />
-      </div>
-      <div>
-        <Checkbox v-model="checked2" label="真偽値1" />
-      </div>
-    </div>
-    <div class="tableWrapper">
-      <table class="customTable">
-        <thead>
-          <th>Table Hader</th>
-          <th>Table Hader</th>
-          <th>Table Hader</th>
-          <th>Table Hader</th>
-          <th>Table Hader</th>
-        </thead>
-        <tbody>
-          <tr>
-            <th>Table Hader</th>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-          </tr>
-          <tr>
-            <th>Table Hader</th>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-          </tr>
-          <tr>
-            <th>Table Hader</th>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-          </tr>
-          <tr>
-            <th>Table Hader</th>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-          </tr>
-          <tr>
-            <th>Table Hader</th>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-            <td>Table Data</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div>
-      <img
-        :src="`https://picsum.photos/id/${pageNumber}/200/200`"
-        alt=""
-        width="200"
-        height="200"
-      />
-      <UsePaginationComponent />
-    </div>
-    <div>
-      <BasicButton
-        label="モーダルを表示"
-        @click="
-          () => {
-            isShown = true;
-          }
-        "
-      />
-      <!-- <UseModalComponent /> -->
-      <Modal
-        v-model="isShown"
-        title="タイトル"
-        text="テキスト"
-        labelPrimary="ボタン1"
-        labelSecondary="ボタン2"
-        :onClickPrimary="modalClick1"
-        :onClickSecondary="modalClick2"
-      />
-    </div>
-
-    <Layout>
-      <div class="layoutBlock colSpan-3">4分の1</div>
-      <div class="layoutBlock colSpan-6">4分の2</div>
-      <div class="layoutBlock colSpan-3">4分の1</div>
-
-      <div class="layoutBlock colSpan-3">4分の1</div>
-      <div class="layoutBlock colSpan-3">4分の1</div>
-      <div class="layoutBlock colSpan-3">4分の1</div>
-      <div class="layoutBlock colSpan-3">4分の1</div>
-
-      <div class="layoutBlock colSpan-9">4分の3</div>
-      <div class="layoutBlock colSpan-3">4分の1</div>
-
-      <div class="layoutBlock colSpan-9">4分の3</div>
-
-      <div class="layoutBlock colSpan-4">3分の1</div>
-      <div class="layoutBlock colSpan-4">3分の1</div>
-      <div class="layoutBlock colSpan-4">3分の1</div>
-    </Layout>
-    <p>タブレットは2列にする</p>
-    <Layout>
-      <div class="layoutBlock colSpan-6 colSpanTablet-1">4分の2</div>
-      <div class="layoutBlock colSpan-6 colSpanTablet-1">4分の2</div>
-    </Layout>
-    <p>真ん中半分だけ使う</p>
-    <Layout>
-      <div class="layoutBlock colStart-4 colEnd-10">4分の2</div>
-    </Layout>
-  </div>
-  <div class="templates">
-    <Layout>
-      <div class="colSpan-9">
-        <Heading :headingLevel="1"> ログイン </Heading>
-        <form action="#">
-          <div class="loginForm">
-            <UseEmailInputComponent />
-            <UsePasswordInputComponent />
+        <Layout>
+          <div class="colSpan-6">
+            <Heading :headingLevel="1" designLevel="XXL">見出しXXL</Heading>
+            <Heading :headingLevel="1" designLevel="XL">見出しXL</Heading>
+            <Heading :headingLevel="1"> 見出しL </Heading>
+            <Heading :headingLevel="2"> 見出しM </Heading>
+            <Heading :headingLevel="3"> 見出しS </Heading>
+            <Heading :headingLevel="4"> 見出しXS </Heading>
+            <Heading :headingLevel="5"> 見出しXXS </Heading>
           </div>
-          <div class="loginButtons">
-            <BasicButton label="ログイン" />
-            <BasicButton label="新規登録" type="secondary" />
-            <BasicButton label="パスワードをお忘れの方" type="tertiary" />
+          <div class="colSpan-6">
+            <p class="fontTextL">本文L：デフォルトの本文テキスト。</p>
+            <p class="fontTextM">
+              本文M：テキストの多いとき、表示領域が限られているときに使用できます。
+            </p>
+            <p class="fontLabeLL">ラベルL：デフォルトのラベルテキスト。</p>
+            <p class="fontLabelM">
+              ラベルM：テキストの多いとき、表示領域が限られているときに使用できます。
+            </p>
+            <p class="fontSupportL">補足L：デフォルトの補足テキスト。</p>
+            <p class="fontSupportM">
+              補足M：テキストの多いとき、表示領域が限られているときに使用できます。
+            </p>
           </div>
-        </form>
+        </Layout>
+      </div>
+      <div class="colSpan-12">
+        <Heading :headingLevel="2"> ボタン </Heading>
+        <Layout>
+          <div class="colSpan-4">
+            <div class="buttonWrapper">
+              <BasicButton label="ボタン" @click="handleClick" />
+
+              <BasicButton label="ボタン" disabled @click="handleClick" />
+            </div>
+          </div>
+          <div class="colSpan-4">
+            <div class="buttonWrapper">
+              <BasicButton
+                label="ボタン"
+                type="secondary"
+                @click="handleClick"
+              />
+              <BasicButton
+                disabled
+                label="ボタン"
+                type="secondary"
+                @click="handleClick"
+              />
+            </div>
+          </div>
+          <div class="colSpan-4">
+            <div class="buttonWrapper">
+              <BasicButton
+                label="ボタン"
+                type="tertiary"
+                @click="handleClick"
+              />
+              <BasicButton
+                disabled
+                label="ボタン"
+                type="tertiary"
+                @click="handleClick"
+              />
+            </div>
+          </div>
+        </Layout>
+      </div>
+      <div class="colSpan-12">
+        <div class="colorScheme">
+          <RadioGroup
+            v-model="colorScheme"
+            groupLabel="テーマカラー"
+            :labels="['ライトモード', 'ダークモード']"
+            :values="['light', 'dark']"
+            helpText="サイトのテーマカラーを選択できます"
+            name="color"
+            :isRequired="false"
+          />
+        </div>
+      </div>
+      <div class="colSpan-12">
+        <Heading :headingLevel="2"> インプット系 </Heading>
+        <Layout>
+          <div class="colSpan-4">
+            <div class="inputWrapper">
+              <FamilyNameInput />
+              <UseTelInputComponent />
+            </div>
+          </div>
+          <div class="colSpan-4">
+            <div class="inputWrapper">
+              <UseTextAreaInputComponent />
+              <MaxCountTextArea />
+            </div>
+          </div>
+          <div class="colSpan-4">
+            <div class="inputWrapper">
+              <div>
+                <RadioGroup
+                  v-model="selected"
+                  groupLabel="ラベル"
+                  :labels="['選択肢1', '選択肢2', '選択肢3']"
+                  :values="['1', '2', '3']"
+                  helpText="ヘルプテキスト"
+                  errorText="エラーテキスト"
+                  name="group"
+                  :isRequired="true"
+                />
+              </div>
+              <div>
+                <RadioGroup
+                  v-model="selected2"
+                  radioStyle="tile"
+                  groupLabel="ラベル"
+                  :labels="['選択肢1', '選択肢2', '選択肢3']"
+                  :subTexts="['補足説明1', '補足説明2', '補足説明3']"
+                  :values="['1', '2', '3']"
+                  helpText="ヘルプテキスト"
+                  errorText="エラーテキスト"
+                  name="group2"
+                  :isRequired="true"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="colSpan-4">
+            <div class="inputWrapper">
+              <CheckboxGroup
+                v-model="checked"
+                groupLabel="ラベル"
+                :labels="['選択肢1', '選択肢2', '選択肢3']"
+                :values="['1', '2', '3']"
+                helpText="ヘルプテキスト"
+                errorText="エラーテキスト"
+                name="group3"
+              />
+            </div>
+            <div>
+              <Checkbox v-model="checked2" label="真偽値1" />
+            </div>
+          </div>
+          <div class="colSpan-12">
+            <Heading :headingLevel="2"> テーブル </Heading>
+            <table class="customTable">
+              <thead>
+                <th>Table Hader</th>
+                <th>Table Hader</th>
+                <th>Table Hader</th>
+                <th>Table Hader</th>
+                <th>Table Hader</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Table Hader</th>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                </tr>
+                <tr>
+                  <th>Table Hader</th>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                </tr>
+                <tr>
+                  <th>Table Hader</th>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                </tr>
+                <tr>
+                  <th>Table Hader</th>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                </tr>
+                <tr>
+                  <th>Table Hader</th>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                  <td>Table Data</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="colSpan-12">
+            <Heading :headingLevel="2"> ページネーション </Heading>
+            <img
+              :src="`https://picsum.photos/id/${pageNumber}/200/200`"
+              alt=""
+              width="200"
+              height="200"
+            />
+            <UsePaginationComponent />
+          </div>
+          <div class="colSpan-12">
+            <Heading :headingLevel="2"> モーダル </Heading>
+            <BasicButton
+              label="モーダルを表示"
+              @click="
+                () => {
+                  isShown = true;
+                }
+              "
+            />
+
+            <Modal
+              v-model="isShown"
+              title="タイトル"
+              text="テキスト"
+              labelPrimary="ボタン1"
+              labelSecondary="ボタン2"
+              :onClickPrimary="modalClick1"
+              :onClickSecondary="modalClick2"
+            />
+          </div>
+          <div class="colSpan-12">
+            <Heading :headingLevel="2"> レイアウト </Heading>
+            <Layout>
+              <div class="layoutBlock colSpan-3">4分の1</div>
+              <div class="layoutBlock colSpan-6">4分の2</div>
+              <div class="layoutBlock colSpan-3">4分の1</div>
+
+              <div class="layoutBlock colSpan-3">4分の1</div>
+              <div class="layoutBlock colSpan-3">4分の1</div>
+              <div class="layoutBlock colSpan-3">4分の1</div>
+              <div class="layoutBlock colSpan-3">4分の1</div>
+
+              <div class="layoutBlock colSpan-9">4分の3</div>
+              <div class="layoutBlock colSpan-3">4分の1</div>
+
+              <div class="layoutBlock colSpan-9">4分の3</div>
+
+              <div class="layoutBlock colSpan-4">3分の1</div>
+              <div class="layoutBlock colSpan-4">3分の1</div>
+              <div class="layoutBlock colSpan-4">3分の1</div>
+            </Layout>
+            <p>タブレットは2列にする</p>
+            <Layout>
+              <div class="layoutBlock colSpan-6 colSpanTablet-1">4分の2</div>
+              <div class="layoutBlock colSpan-6 colSpanTablet-1">4分の2</div>
+            </Layout>
+            <p>真ん中半分だけ使う</p>
+            <Layout>
+              <div class="layoutBlock colStart-4 colEnd-10">4分の2</div>
+            </Layout>
+          </div>
+        </Layout>
       </div>
     </Layout>
+    <div class="templates">
+      <Heading :headingLevel="1" designLevel="XL"> テンプレート </Heading>
+      <hr />
+      <Layout>
+        <div class="colSpan-9">
+          <Heading :headingLevel="1"> ログイン </Heading>
+          <form action="#">
+            <div class="loginForm">
+              <UseEmailInputComponent />
+              <UsePasswordInputComponent />
+            </div>
+            <div class="loginButtons">
+              <BasicButton label="ログイン" />
+              <BasicButton label="新規登録" type="secondary" />
+              <BasicButton label="パスワードをお忘れの方" type="tertiary" />
+            </div>
+          </form>
+        </div>
+      </Layout>
+    </div>
   </div>
 </template>
 
@@ -317,8 +386,19 @@ watch(colorScheme, (color) => {
   }
 }
 
+hr {
+  border-color: var(--color-border-divider);
+}
+
 .colorScheme {
   margin: 36px 0;
+}
+
+.buttonWrapper {
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+  width: 300px;
 }
 
 .inputWrapper {
