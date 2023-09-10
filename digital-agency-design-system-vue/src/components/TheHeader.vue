cbefafecc1594fdfb5639ea48e0fc6ac9c645b55
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-09-10 14:45:00 +0900

    ヘッダーコンポーネントの着手

---
+++ digital-agency-design-system-vue/src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+type Props = {
+  type?: "full" | "slim";
+};
+withDefaults(defineProps<Props>(), {
+  type: "full",
+});
+</script>
+<template>
+  <header :class="type">
+    <slot name="logo"></slot>
+    <slot name="item"></slot>
+    <slot name="menu"></slot>
+    <slot name="hamburger"></slot>
+  </header>
+</template>
+<style scoped lang="scss">
+header {
+  display: grid;
+}
+</style>
