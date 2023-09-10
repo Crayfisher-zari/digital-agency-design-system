26dd1eb8b654806d9f8a74ef851e76a0a924acb1
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-09-10 14:45:00 +0900

    ハンバーガーボタンの見た目の調整

--- digital-agency-design-system-vue/src/components/HamburgerButton.vue
+++ digital-agency-design-system-vue/src/components/HamburgerButton.vue
@@ -2,6 +2,7 @@
 import { computed } from "vue";
 import Icon from "./Icon.vue";
 import iconHamburger from "@/assets/images/icon_hamburger.svg";
+import iconClose from "@/assets/images/icon_close.svg";
 
 type Props = {
   type?: "vertical" | "horizontal";
@@ -35,22 +36,44 @@ const state = computed({
 </script>
 <template>
   <button class="hamburgerButton" :class="type" @click="handleInput">
-    <Icon
-      :iconSrc="iconHamburger"
-      :width="18"
-      :height="14"
-      color="var(--color-text-body)"
-    />
-    <span class="text">
-      {{ modelValue ? "閉じる" : "メニュー" }}
-    </span>
+    <Transition mode="out-in">
+      <span v-show="!modelValue" class="buttonInner">
+        <span class="iconWrapper">
+          <Icon
+            :iconSrc="iconHamburger"
+            :width="18"
+            :height="14"
+            color="var(--color-text-body)"
+            class="icon hamburger"
+          />
+        </span>
+        <span class="text">メニュー</span>
+      </span>
+    </Transition>
+    <Transition mode="out-in">
+      <span v-show="modelValue" class="buttonInner">
+        <span class="iconWrapper">
+          <Icon
+            v-show="modelValue"
+            :iconSrc="iconClose"
+            :width="14"
+            :height="14"
+            color="var(--color-text-body)"
+            class="icon close"
+          />
+        </span>
+        <span class="text">閉じる</span>
+      </span>
+    </Transition>
   </button>
 </template>
 <style lang="scss" scoped>
 @use "@/assets/style/utils/utils.scss" as *;
 
 .hamburgerButton {
+  position: relative;
   appearance: none;
+  cursor: pointer;
   background-color: transparent;
   border: none;
 
@@ -60,10 +83,53 @@ const state = computed({
   }
 }
 
+.buttonInner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+
+  &.v-enter-active,
+  .v-leave-active {
+    transition: opacity var(--base-duration) var(--easing-out-quat);
+  }
+
+  &.v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+}
+
 .text {
   display: block;
   font-size: pxToRem(10);
   color: var(--color-text-body);
   text-align: center;
+}
+
+.iconWrapper {
+  position: relative;
+  display: block;
+  width: 24px;
+  height: 24px;
+  margin-top: 5px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.icon {
+  position: absolute;
+  top: 5px;
+  height: auto;
+
+  &.hamburger {
+    left: 3px;
+  }
+
+  &.close {
+    left: 5px;
+  }
 }
 </style>
