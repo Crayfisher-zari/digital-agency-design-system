dc8d58b0fe8fbb6e50600c1dcb9b3efe8b6adb55
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-09-10 18:03:13 +0900

    Drawerコンポーネントの着手

---
+++ digital-agency-design-system-vue/src/components/Drawer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template><div class="drawer"></div></template>
+<style lang="scss" scoped>
+@use "@/assets/style/utils/utils.scss" as *;
+
+.drawer {
+  @include mediaQueryUp {
+    display: none;
+  }
+}
+</style>
