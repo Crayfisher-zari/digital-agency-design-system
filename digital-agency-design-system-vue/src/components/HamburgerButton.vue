3847b511ed1eae6ea628206c408dd70afa8acab3
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-09-10 14:45:00 +0900

    ハンバーガーボタンのコンポーネントの着手

---
+++ digital-agency-design-system-vue/src/components/HamburgerButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+type Props = {
+  type?: "vertical" | "horizontal";
+  onClick?: () => void;
+};
+type Emits = { (e: "update:modelValue", value: [] | string[] | boolean): void };
+
+const props = withDefaults(defineProps<Props>(), {
+  type: "vertical",
+});
+
+const handleInput = ()=>{}
+
+const emit = defineEmits<Emits>();
+
+const state = computed({get:()=>{},set:()=>{}})
+</script>
+<template>
+  <button @click="handleInput">メニュー</button>
+</template>
+<style lang="scss" scoped></style>
