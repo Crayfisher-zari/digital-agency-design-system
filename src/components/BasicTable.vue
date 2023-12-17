3c44e3d258808d999e7350cd6f0a7bba88c30126
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-12-17 21:54:14 +0900

    テーブルをコンポーネント化

---
+++ src/components/BasicTable.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tableWrapper">
+    <slot></slot>
+  </div>
+</template>
+<style scoped lang="scss">
+/* テーブルの基本スタイル */
+.tableWrapper {
+  display: inline-block;
+  max-width: 100%;
+  overflow: auto;
+  &:deep(table){
+    font-size: pxToRem(16);
+    color: var(--color-text-body);
+    table-layout: fixed;
+    border-collapse: collapse;
+  }
+  &:deep(th),&:deep(td){
+    min-width: 190px;
+    height: 80px;
+    padding: 8px;
+    text-align: left;
+    border: 1px solid var(--color-border-divider);
+  }
+  &:deep(th){
+    font-weight: normal;
+    background-color: var(--color-background-tertiary);
+  }
+}
+</style>
