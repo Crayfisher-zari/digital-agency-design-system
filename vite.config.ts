1806c85102fd71ea11af29ee58d0b90b13de2bcb
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2023-12-17 13:33:37 +0900

    dotenvの導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import 'dotenv/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/digital-agency-design-system/",
+  base: process.env?.BASE_PATH ?? "/",
   resolve: {
     alias: {
       "@": `${__dirname}/src/`,
