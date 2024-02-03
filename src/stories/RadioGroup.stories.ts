edfde7c4ea4eaef21b17edecf050e5a2dd75b36a
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2024-02-03 13:49:46 +0900

    ラジオグループのStory作成

--- src/stories/RadioGroup.stories.ts
+++ src/stories/RadioGroup.stories.ts
@@ -1,20 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/vue3";
-import CheckboxGroup from "../components/CheckboxGroup.vue";
+import RadioGroup from "../components/RadioGroup.vue";
 import { ref } from "vue";
 
 const meta = {
-  title: "CheckboxGroup",
-  component: CheckboxGroup,
-  tags: ["autodocs"],
+  title: "RadioGroup",
+  component:RadioGroup,
+  tags:["autodocs"],
   argTypes: {
-    groupLabel: {
+    groupLabel:{
       control: "text",
     },
-    supportText: {
-      control: "text",
-    },
-    errorText: {
-      control: "text",
+    radioStyle:{
+      control: "radio",
+      options: ["default", "tile"],
     },
     isRequired: {
       control: "boolean",
@@ -37,22 +35,24 @@ const meta = {
     isValid: true,
     isDisabled: false,
   },
-} as Meta<typeof CheckboxGroup>;
+
+}as Meta<typeof RadioGroup>;
 type Story = StoryObj<typeof meta>;
+
 
 export default meta;
 
-export const Base: Story = {
-  render: (args) => ({
-    components: { CheckboxGroup },
-    setup: () => {
-      const checked = ref([]);
-      return { checked, ...args };
+export const Base:Story = {
+  render:(args)=>({
+    components:{RadioGroup},
+    setup:()=>{
+      const checked = ref();
+      return {checked,...args}
     },
-    template: `
-      <div>
-      <p class="mb">チェックされたもの：{{ checked }}</p>
-        <CheckboxGroup
+    template:`
+    <div>
+      <p class="mb">選択されたもの：{{ checked }}</p>
+        <RadioGroup
           v-model="checked"
           :groupLabel="groupLabel"
           :labels="labels"
@@ -61,9 +61,9 @@ export const Base: Story = {
           :errorText="errorText"
           :isValid="isValid"
           :isRequired="isRequired"
-          name="group3"
+          name="radio-group"
         />
       </div>
-    `,
-  }),
-};
+    `
+  })
+}
