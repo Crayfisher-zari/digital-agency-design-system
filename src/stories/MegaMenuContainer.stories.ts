12edf69a5ed586a08b263524777b00e0d771de0c
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2024-02-12 21:39:25 +0900

    メガメニューのストーリー作成

---
+++ src/stories/MegaMenuContainer.stories.ts
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import MegaMenuContainer from "../components/MegaMenuContainer.vue";
+import MegaMenuTitle from "../components/MegaMenuTitle.vue";
+import Menu from "../components/Menu.vue";
+import {menuList} from "./stub/menulist";
+
+const meta = {
+  title: "メガメニューコンテナー",
+  component: MegaMenuContainer,
+  tags: ["autodocs"],
+  argTypes: {
+    style: {
+      control: "radio",
+      options: ["flat", "shadow"],
+    },
+    isVisible: {
+      control: "boolean",
+    },
+  },
+  args: {
+    style: "flat",
+    isVisible: true,
+  },
+} satisfies Meta<typeof MegaMenuContainer>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+/**
+ * メガメニューコンテナーはレイアウトを提供します
+ */
+export const base: Story = {
+  args: {
+    style: "flat",
+    isVisible: true,
+  },
+  render: (args) => ({
+    components: { MegaMenuContainer, MegaMenuTitle, Menu },
+    setup: () => {
+      return { ...args,menuList };
+    },
+    template: `
+    <MegaMenuContainer :isVisible="isVisible">
+      <template #col1>
+        <MegaMenuTitle
+          title="メガメニュー"
+          text="サブタイトルはここに記入します"
+          linkText="2階層目へのリンク"
+          linkUrl="#!"
+        />
+      </template>
+      <template #col2>
+        <Menu :menuList="menuList" :hasGap="false" linkTag="a"></Menu>
+      </template>
+      <template #col3>
+        <Menu :menuList="menuList" :hasGap="false" linkTag="a"></Menu>
+      </template>
+    </MegaMenuContainer>
+    `,
+  }),
+};
