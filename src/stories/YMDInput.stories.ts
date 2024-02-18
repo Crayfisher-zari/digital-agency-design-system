cb4bba88c4f7c8db99b3530ed04b596d0c6ac7b6
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2024-02-18 16:55:44 +0900

    年月日コンポーネントのストーリー作成

---
+++ src/stories/YMDInput.stories.ts
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import YMDInput from "../components/YMDInput.vue";
+
+const meta = {
+  title: "年月日テキストエリア",
+  component: YMDInput,
+  tags: ["autodocs"],
+  argTypes: {
+    year: {
+      control: "text",
+    },
+    month: {
+      control: "text",
+    },
+    day: {
+      control: "text",
+    },
+    label: {
+      control: "text",
+    },
+    supportText: {
+      control: "text",
+    },
+    errorText: {
+      control: "text",
+    },
+    isRequired: {
+      control: "boolean",
+    },
+    isValid: {
+      control: "boolean",
+    },
+    isDisabled: {
+      control: "boolean",
+    },
+  },
+  args: {
+    year: "1980",
+    month: "1",
+    day: "1",
+    label: "生年月日",
+    supportText: "月を選択してから日を選んでください",
+  },
+} satisfies Meta<typeof YMDInput>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  args: {
+    year: "",
+    month: "",
+    day: "",
+    label: "生年月日",
+    supportText: "月を選択してから日を選んでください",
+  },
+};
---
+++ src/stories/YMDSelector.stories.ts
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import YMDSelctor from "../components/YMDSelector.vue";
+
+const meta = {
+  title: "年月日セレクター",
+  component: YMDSelctor,
+  tags: ["autodocs"],
+  argTypes: {
+    year: {
+      control: "number",
+    },
+    month: {
+      control: "number",
+    },
+    day: {
+      control: "number",
+    },
+    defaultYear: {
+      control: "number",
+    },
+    yearList: {
+      control: "object",
+      options: [2020, 2021, 2022, 2023, 2024],
+    },
+    label: {
+      control: "text",
+    },
+    supportText: {
+      control: "text",
+    },
+    errorText: {
+      control: "text",
+    },
+    isRequired: {
+      control: "boolean",
+    },
+    isValid: {
+      control: "boolean",
+    },
+    isDisabled: {
+      control: "boolean",
+    },
+  },
+  args: {},
+} satisfies Meta<typeof YMDSelctor>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  args: {
+    year: null,
+    month: null,
+    day: null,
+    label: "生年月日",
+    supportText: "月を選択してから日を選んでください",
+  },
+};
