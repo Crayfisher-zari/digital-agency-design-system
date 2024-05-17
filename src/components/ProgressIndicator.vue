e73dad4eb11be64257aa15914f69b8128930272d
Nishihara Tsubasa <95861046+Crayfisher-zari@users.noreply.github.com>
2024-05-17 10:11:08 +0900

    プログレスインジケーターの着手

---
+++ src/components/ProgressIndicator.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import spinner from "@/assets/images/icon_spinner.svg";
+type Props = {
+  style: "spinner" | "linear";
+  label?: string;
+  isOverlay?: boolean;
+  labelPosition?: "stacked" | "inline";
+};
+withDefaults(defineProps<Props>(), {
+  label: undefined,
+  isOverlay: false,
+  labelPosition: "stacked",
+});
+</script>
+<template>
+  <div>
+    <div v-if="style === 'spinner'">
+      <img :src="spinner" alt="" width="48" height="48" />
+    </div>
+    <p class="label">{{ label }}</p>
+  </div>
+</template>
+<style lang="scss" scoped>
+@use "@/assets/style/utils/utils.scss" as *;
+
+.label {
+  font-size: pxToRem(16);
+  line-height: 1.7;
+  color: var(--color-text-placeHolder);
+}
+</style>
---
+++ src/stories/ProgressIndicator.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import ProgressIndicator from "../components/ProgressIndicator.vue";
+
+const meta = {
+  title: "プログレスインジケーター",
+  component: ProgressIndicator,
+  tags: ["autodocs"],
+  argTypes: {
+    style: {
+      control: "radio",
+      options: ["spinner", "liear"],
+    },
+    label: {
+      control: "text",
+    },
+    labelPosition: {
+      control: "radio",
+      options: ["stacked", "inline"],
+    },
+    isOverlay: {
+      control: "boolean",
+    },
+  },
+  args: {
+    style: "spinner",
+    label: "ラベル",
+    isOverlay: false,
+  },
+} satisfies Meta<typeof ProgressIndicator>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  style: "spinner",
+  label: "ラベル",
+  isOverlay: false,
+};
