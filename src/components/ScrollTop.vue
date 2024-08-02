befa6c7f6c99ba486502bf4e846387e2957832d0
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2024-08-02 09:58:50 +0900

    スクロールトップボタンの作成

--- src/components/ScrollTop.vue
+++ src/components/ScrollTop.vue
@@ -1,5 +1,9 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+import Icon from "./Icon.vue";
+import iconArrowUpwards from "@/assets/images/icon_arrow_upward.svg";
+
 type Props = {
+  /** クリック時の処理 */
   onClick?: () => void;
 };
 
@@ -20,6 +24,12 @@ const handleClick = () => {
 </script>
 <template>
   <button class="scrollTopButton" @click="handleClick">
+    <Icon
+      :iconSrc="iconArrowUpwards"
+      :width="24"
+      :height="24"
+      color="var(--icon-color)"
+    />
     <div class="sr-only">ページ上部に戻る</div>
   </button>
 </template>
@@ -27,6 +37,25 @@ const handleClick = () => {
 .scrollTopButton {
   width: 56px;
   height: 56px;
+  cursor: pointer;
+  background-color: var(--color-background-primary);
+  border: 1px solid var(--color-button-normal);
   border-radius: 28px;
+  transition:
+    background-color var(--base-duration) var(--easing-out-quat),
+    border-color var(--base-duration) var(--easing-out-quat);
+
+  --icon-color: var(--color-button-normal);
+
+  &:hover {
+    background-color: var(--color-button-secondary-hover);
+  }
+
+  &:active {
+    background-color: var(--color-button-secondary-active);
+    border-color: var(--color-button-active);
+
+    --icon-color: var(--color-button-active);
+  }
 }
 </style>
