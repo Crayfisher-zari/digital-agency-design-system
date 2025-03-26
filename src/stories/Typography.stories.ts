3db574ac1001a851814714f024a511609d883b76
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2025-03-26 15:26:24 +0900

    タイポグラフィのアップデート

--- src/stories/Typography.stories.ts
+++ src/stories/Typography.stories.ts
@@ -15,87 +15,96 @@ export const Base: Story = {
     template: `
     <h1 class="story-book-preview-mb-medium">Display (Dsp)</h1>
     <div class="story-book-preview-row-layout story-book-preview-mb-large">
-      <p class="Dsp-64N-4">Dsp-64N-4</p>
-      <p class="Dsp-57N-4">Dsp-57N-4</p>
-      <p class="Dsp-48N-4">Dsp-48N-4</p>
+      <p class="Dsp-64N-140">Dsp-64N-140</p>
+      <p class="Dsp-57N-140">Dsp-57N-140</p>
+      <p class="Dsp-48N-140">Dsp-48N-140</p>
     </div>
 
     <h1 class="story-book-preview-mb-medium">Standard (Std) </h1>
     <h2 class="Std-28B-5 story-book-preview-mb">Bold</h2>
     <div class="story-book-preview-row-layout story-book-preview-mb-large">
-      <p class="Std-45B-4">Std-45B-4</p>
-      <p class="Std-36B-4">Std-36B-4</p>
-      <p class="Std-28B-5">Std-28B-5</p>
-      <p class="Std-24B-5">Std-24B-5</p>
-      <p class="Std-22B-5">Std-22B-5</p>
-      <p class="Std-20B-5">Std-20B-5</p>
-      <p class="Std-18B-6">Std-18B-6</p>
-      <p class="Std-17B-7">Std-17B-7</p>
-      <p class="Std-16B-7">Std-16B-7</p>
-    </div>
-
-    <h2 class="Std-28B-5 story-book-preview-mb">Medium</h2>
-    <div class="story-book-preview-row-layout story-book-preview-mb-large">
-      <p class="Std-32M-5">Std-32M-5</p>
-      <p class="Std-28M-5">Std-28M-5</p>
-      <p class="Std-24M-5">Std-24M-5</p>
-      <p class="Std-20M-5">Std-20M-5</p>
-      <p class="Std-17M-7">Std-17M-7</p>
-      <p class="Std-16M-7">Std-16M-7</p>
+      <p class="Std-45B-140">Std-45B-140</p>
+      <p class="Std-36B-140">Std-36B-140</p>
+      <p class="Std-32B-150">Std-32B-150</p>
+      <p class="Std-28B-150">Std-28B-150</p>
+      <p class="Std-26B-150">Std-26B-150</p>
+      <p class="Std-24B-150">Std-24B-150</p>
+      <p class="Std-22B-150">Std-22B-150</p>
+      <p class="Std-20B-150">Std-20B-150</p>
+      <p class="Std-18B-160">Std-18B-160</p>
+      <p class="Std-17B-170">Std-17B-170</p>
+      <p class="Std-16B-170">Std-16B-170</p>
+      <p class="Std-16B-175">Std-16B-175</p>
     </div>
 
     <h2 class="Std-28B-5 story-book-preview-mb">Normal</h2>
     <div class="story-book-preview-row-layout story-book-preview-mb-large">
-      <p class="Std-45N-4">Std-45N-4</p>
-      <p class="Std-36N-4">Std-36N-4</p>
-      <p class="Std-32N-5">Std-32N-5</p>
-      <p class="Std-28N-5">Std-28N-5</p>
-      <p class="Std-26N-5">Std-26N-5</p>
-      <p class="Std-24N-5">Std-24N-5</p>
-      <p class="Std-22N-5">Std-22N-5</p>
-      <p class="Std-20N-5">Std-20N-5</p>
-      <p class="Std-18N-6">Std-18N-6</p>
-      <p class="Std-17N-7">Std-17N-7</p>
-      <p class="Std-16N-7">Std-16N-7</p>
+      <p class="Std-45N-140">Std-45N-140</p>
+      <p class="Std-36N-140">Std-36N-140</p>
+      <p class="Std-32N-150">Std-32N-150</p>
+      <p class="Std-28N-150">Std-28N-150</p>
+      <p class="Std-26N-150">Std-26N-150</p>
+      <p class="Std-24N-150">Std-24N-150</p>
+      <p class="Std-22N-150">Std-22N-150</p>
+      <p class="Std-20N-150">Std-20N-150</p>
+      <p class="Std-18N-160">Std-18N-160</p>
+      <p class="Std-17N-170">Std-17N-170</p>
+      <p class="Std-16N-170">Std-16N-170</p>
+      <p class="Std-16N-175">Std-16N-175</p>
     </div>
 
     <h1 class="story-book-preview-mb-medium">Dense (Dns) </h1>
+
+    <h2 class="Std-28B-5 story-book-preview-mb">Bold</h2>
     <div class="story-book-preview-row-layout story-book-preview-mb-large">
-      <p class="Dns-17B-3">Dns-17B-3</p>
-      <p class="Dns-17N-3">Dns-17N-3</p>
-      <p class="Dns-17B-2">Dns-17B-2</p>
-      <p class="Dns-17N-2">Dns-17N-2</p>
-      <p class="Dns-16B-3">Dns-16B-3</p>
-      <p class="Dns-16N-3">Dns-16N-3</p>
-      <p class="Dns-16B-2">Dns-16B-2</p>
-      <p class="Dns-16N-2">Dns-16N-2</p>
-      <p class="Dns-14B-3">Dns-14B-3</p>
-      <p class="Dns-14N-3">Dns-14N-3</p>
-      <p class="Dns-14B-2">Dns-14B-2</p>
-      <p class="Dns-14N-2">Dns-14N-2</p>
+      <p class="Dns-17B-130">Dns-17B-130</p>
+      <p class="Dns-17B-120">Dns-17B-120</p>
+      <p class="Dns-16B-130">Dns-16B-130</p>
+      <p class="Dns-16B-120">Dns-16B-120</p>
+      <p class="Dns-14B-130">Dns-14B-130</p>
+      <p class="Dns-14B-120">Dns-14B-120</p>
+    </div>
+
+    <h2 class="Std-28B-5 story-book-preview-mb">Normal</h2>
+    <div class="story-book-preview-row-layout story-book-preview-mb-large">
+      <p class="Dns-17N-130">Dns-17N-130</p>
+      <p class="Dns-17N-120">Dns-17N-120</p>
+      <p class="Dns-16N-130">Dns-16N-130</p>
+      <p class="Dns-16N-120">Dns-16N-120</p>
+      <p class="Dns-14N-130">Dns-14N-130</p>
+      <p class="Dns-14N-120">Dns-14N-120</p>
     </div>
 
     <h1 class="story-book-preview-mb-medium">Oneline (Oln) </h1>
+
+    <h2 class="Std-28B-5 story-book-preview-mb">Bold</h2>
     <div class="story-book-preview-row-layout story-book-preview-mb-large">
-      <p class="Oln-17B-1">Oln-17B-1</p>
-      <p class="Oln-17M-1">Oln-17M-1</p>
-      <p class="Oln-17N-1">Oln-17N-1</p>
-      <p class="Oln-16B-1">Oln-16B-1</p>
-      <p class="Oln-16M-1">Oln-16M-1</p>
-      <p class="Oln-16N-1">Oln-16N-1</p>
-      <p class="Oln-14B-1">Oln-14B-1</p>
-      <p class="Oln-14M-1">Oln-14M-1</p>
-      <p class="Oln-14N-1">Oln-14N-1</p>
+      <p class="Oln-17B-100">Oln-17B-100</p>
+      <p class="Oln-16B-100">Oln-16B-100</p>
+      <p class="Oln-14B-100">Oln-14B-100</p>
+    </div>
+
+    <h2 class="Std-28B-5 story-book-preview-mb">Noramal</h2>
+    <div class="story-book-preview-row-layout story-book-preview-mb-large">
+      <p class="Oln-17N-100">Oln-17N-100</p>
+      <p class="Oln-16N-100">Oln-16N-100</p>
+      <p class="Oln-14N-100">Oln-14N-100</p>
     </div>
 
     <h1 class="story-book-preview-mb-medium">Mono (Mono) </h1>
+
+    <h2 class="Std-28B-5 story-book-preview-mb">Noramal</h2>
     <div class="story-book-preview-row-layout story-book-preview-mb-large">
-      <p class="Mono-17B-5">Mono-17B-5</p>
-      <p class="Mono-17N-5">Mono-17N-5</p>
-      <p class="Mono-16B-5">Mono-16B-5</p>
-      <p class="Mono-16N-5">Mono-16N-5</p>
-      <p class="Mono-14B-5">Mono-14B-5</p>
-      <p class="Mono-14N-5">Mono-14N-5</p>
+      <p class="Mono-17B-150">Mono-17B-150</p>
+      <p class="Mono-16B-150">Mono-16B-150</p>
+      <p class="Mono-14B-150">Mono-14B-150</p>
+    </div>
+
+    <h2 class="Std-28B-5 story-book-preview-mb">Noramal</h2>
+    <div class="story-book-preview-row-layout story-book-preview-mb-large">
+      <p class="Mono-17N-150">Mono-17N-150</p>
+      <p class="Mono-16N-150">Mono-16N-150</p>
+      <p class="Mono-14N-150">Mono-14N-150</p>
     </div>
     `,
   }),
