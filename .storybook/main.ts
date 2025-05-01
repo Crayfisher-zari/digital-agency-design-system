a155fc23084ef4509c225980d66e0f0a60e759c8
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2025-05-01 21:34:39 +0900

    ファイルの修正

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/add",
     "storybook-dark-mode",
     "@storybook/addon-viewport",
   ],
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,39 +1,37 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginOxlint from 'eslint-plugin-oxlint'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-import storybook from 'eslint-plugin-storybook'
-
-
-
-
+import { globalIgnores } from "eslint/config";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from "@vue/eslint-config-typescript";
+import pluginVue from "eslint-plugin-vue";
+import pluginOxlint from "eslint-plugin-oxlint";
+import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import storybook from "eslint-plugin-storybook";
 
 const vueConfig = defineConfigWithVueTs(
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    name: "app/files-to-lint",
+    files: ["**/*.{ts,mts,tsx,vue}"],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(["**/dist/**", "**/dist-ssr/**", "**/coverage/**"]),
 
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
-  ...pluginOxlint.configs['flat/recommended'],
+  ...pluginOxlint.configs["flat/recommended"],
   skipFormatting,
-
-)
-
+);
 
 export default [
+  globalIgnores(["./src/vite-env.d.ts"]),
   ...vueConfig,
   {
-    files:["**/*.vue"],
-    rules:{
+    files: ["**/*.vue"],
+    rules: {
       "vue/attribute-hyphenation": 0,
       "vue/multi-word-component-names": 0,
-      "vue/v-on-event-hyphenation": 0
-    }
+      "vue/v-on-event-hyphenation": 0,
+    },
   },
-  ...storybook.configs['flat/recommended'],
-]
+  ...storybook.configs["flat/recommended"],
+];
