799f1f65e876d56e26f0a3e90a2759a02d8c1337
Nishihara Tsubasa <95861046+Crayfisher-zari@users.noreply.github.com>
2025-12-13 23:58:36 +0900

    円形のスタイル作成

--- src/components/StepNavigationItem.vue
+++ src/components/StepNavigationItem.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 defineProps<{
   step: number;
+  size?: "small" | "medium";
   process: "deafault" | "skipped" | "reached" ;
   state?: "editing" | "error" ;
 }>();
 </script>
 
 <template>
-  <div class="stepNavigationItem">
+  <div class="stepNavigationItem" :class="size">
     <div class="stepNumber">
       {{ step }}
     </div>
@@ -15,4 +16,22 @@ defineProps<{
 </template>
 
 <style scoped>
+  .stepNavigationItem{
+    display: grid;
+    place-items: center;
+    width: 44px;
+    height: 44px;
+    border-radius: 50%;
+    border: 2px solid var(--color-text-secondary);
+    &.small{
+      width: 32px;
+      height: 32px;
+    }
+  }
+  .stepNumber{
+    font-size: 1.25rem;
+    font-weight: var(--weight-bold);
+    line-height: 1;
+    color: var(--color-text-secondary);
+  }
 </style>
